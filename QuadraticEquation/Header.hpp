9504e6903f0047859edb26d6c0e524c2d0d3a109
KateZin <90635745+KateZin@users.noreply.github.com>
2022-03-16 04:31:01 +0300

    edited

--- QuadraticEquation/Header.hpp
+++ QuadraticEquation/Header.hpp
@@ -3,5 +3,13 @@
 #include<cmath>
 using namespace std;
 
+typedef enum roots {
+	noRoots,
+	oneRoot,
+	twoRoots
+}roots;
+
+void EnterCoefficient(double& a, double& b, double& c);
 double Disriminant(double a, double b, double c);
-char SolveEquation(double a, double b, double c, double& x1, double& x2);
+roots SolveEquation(const double a, const double b, const double c, double& x1, double& x2);
+void PrintResult(roots amountOfRoots, const double x1, const double x2);
--- QuadraticEquation/SolveEq.cpp
+++ QuadraticEquation/SolveEq.cpp
@@ -1,40 +1,52 @@
-#include<Header.hpp>
+#include "Header.hpp"
 
 double Disriminant(double a, double b, double c) {
 	return b * b - 4 * a * c;
 }
 
-char SolveEquation(double a, double b, double c, double& x1, double& x2) {
+roots SolveEquation(const double a, const double b, const double c, double& x1, double& x2) {
 	double d = Disriminant(a, b, c);
 	if (d < 0) {
-		return -1;
+		return noRoots;
 	}
 	else if (d == 0) {
 		x1 = x2 = (-b) / 2 * a;
-		return 0;
+		return oneRoot;
 	}
 	else {
 		x1 = (-b + sqrt(d)) / (2 * a);
 		x2 = (-b - sqrt(d)) / (2 * a);
-		return 1;
+		return twoRoots;
 	}
 }
 
-int main() {
-	double a, b, c, x1, x2;
-	char flag;
+void EnterCoefficient(double &a, double &b, double &c) {
 	cout << "Enter coefficients of quadratic equation ax^2 + bx + c = 0 (a is not quel to 0):" << endl;
-	cin >> a >> b >> c;
-	flag = SolveEquation(a, b, c, x1, x2);
+	cout << "a = " ;
+	cin >> a;
+	cout << "b = ";
+	cin >> b;
+	cout << "c = ";
+	cin >> c;
+}
+
+void PrintResult(roots amountOfRoots, const double x1, const double x2) {
 	cout << "The result is:" << endl;
-	if (flag == -1) {
+	if (amountOfRoots == noRoots) {
 		cout << "no real roots";
 	}
-	else if (flag == 0) {
+	else if (amountOfRoots == oneRoot) {
 		cout << "roots are equal: x1 = x2 = " << x1;
 	}
 	else {
 		cout << "x1 = " << x1 << ";  x2 = " << x2;
 	}
-	
+}
+
+int main() {
+	double a, b, c, x1, x2;
+	roots amountOfRoots;
+	EnterCoefficient(a, b, c);
+	amountOfRoots = SolveEquation(a, b, c, x1, x2);
+	PrintResult(amountOfRoots, x1, x2);
 }
